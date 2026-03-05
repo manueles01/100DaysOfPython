a043f1a69728065ca1cc0a9933ec12c0ecf90714
My Lord Chipi Chipi <mylordchipichipi@Mys-MacBook-Pro.local>
2026-03-05 13:43:16 -0500

    added logic to bring an umbrella

--- will_It_rain/main.py
+++ will_It_rain/main.py
@@ -9,9 +9,18 @@ weather_params ={
     "lat": MY_LAT,
     "lon":MY_LON,
     "appid":api_key,
+    "cnt": 4,
 }
 
 
 response = requests.get(OWM_Endpoint, params=weather_params)
-print(response.status_code)
-print(response.json())
+response.raise_for_status()
+weather_data = response.json()
+print(weather_data["list"][0]["weather"][0]["id"])
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) <700: #and int(condition_code)>200:
+        will_rain = True
+    if will_rain:
+        print("bring an umbrella")
